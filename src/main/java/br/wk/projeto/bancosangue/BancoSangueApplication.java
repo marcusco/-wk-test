989834f488f118c5038e358a21bcd8b564964520
marcusco <marcuscesaro@gmail.com>
2025-01-27 14:06:02 -0300

    Commit inicial

---
+++ src/main/java/br/wk/projeto/bancosangue/BancoSangueApplication.java
@@ -0,0 +1,13 @@
+package br.wk.projeto.bancosangue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BancoSangueApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BancoSangueApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/wk/projeto/bancosangue/BancoSangueApplicationTests.java
@@ -0,0 +1,13 @@
+package br.wk.projeto.bancosangue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BancoSangueApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
